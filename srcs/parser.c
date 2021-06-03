356d7832f859c3a1f22e411934c7c54df837db87
scilla <scilla@student.42.fr>
2021-06-03 19:23:11 +0200

    up

--- srcs/parser.c
+++ srcs/parser.c
@@ -4,26 +4,74 @@
 **	store the commands that will be executed.
 */
 
-#include "../includes/parser.h"
+// #include "../includes/parser.h"
 #include "../includes/minishell.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct	s_cmd
+#define IN_QUOTE 1
+#define IN_DQUOTE 2
+
+typedef struct	s_comm
 {
 	char			**arr;
 	int				len;
 	struct s_cmd	*next;
 	int				next_type;
 	FILE			*file_in;
-}				t_cmd;
+}				t_comm;
 
-void	start_parsing(char *cmd)
+char	**append_to_arr(const char *str, int *len, char **arr)
 {
-	t_cmd	*cmd_table;
 	char	**tmp;
+	int		i;
+	int		n;
 
-	
+	n = *len;
+	tmp = malloc(sizeof(char *) * (n + 1));
+	i = 0;
+	while (i < n)
+	{
+		tmp[i] = arr[i];
+		i++;
+	}
+	// remove system strdup
+	tmp[i] = strdup(str);
+	free(arr);
+	(*len)++;
+	return (tmp);
+}
+
+typedef struct	s_flags
+{
+	int		in_dquote;
+	int		in_quote;
+	int		is_var;
+}				t_flags;
+
+void	expand_env(char *str)
+{
+	int	is_quote;
+	int	is_dquote;
+
+	is_dquote = 0;
+	is_quote = 0;
+}
+
+void	start_parsing(const char *cmd)
+{
+	t_comm	*cmd_table;
+	char	**arr;
+	int		*to_expand;
+	int		len;
+	char	**spc_spl;
+
+	arr = malloc(0);
+	len = 0;
+	spc_spl = ft_split(cmd, ' ');
+
 }
 
 int	main(int argv, char **argc)
@@ -42,4 +90,15 @@ cmd >> file_app
 cmd < file_in
 cmd1 ; cmd2
 
+bash-3.2$ echo "$TERM"
+xterm-256color
+
+bash-3.2$ echo '$TERM'
+$TERM
+
+bash-3.2$ echo "\'"
+\'
+
+bash-3.2$ echo '\"'
+\"
 */
