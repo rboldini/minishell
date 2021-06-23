82529c8d055f2ccbaf7cdcbee2e517f0b4c644cb
buonve <buonvino.vincenzo@gmail.com>
2021-06-23 14:50:32 +0200

    risolto bug ctrl+C doppio prompt

--- srcs/prompt/ft_signal.c
+++ srcs/prompt/ft_signal.c
@@ -4,14 +4,19 @@ void	ft_signal(int sig)
 {
 	if (sig == 2)
 	{
-		ft_bzero(minishell->current->row, ft_strlen(minishell->current->row));
-		ft_strlcpy(minishell->current->row, minishell->current->old, ft_strlen(minishell->current->old) + 1);
-		minishell->current->index = ft_strlen(minishell->current->row);
-		write(1, "\n", 1);
-		write(1, getprompt(minishell), ft_strlen(getprompt(minishell)));
-		while (minishell->current->next)
-			minishell->current = minishell->current->next;
-		minishell->current->index = 0;
+		if(!minishell->pid)
+		{
+			ft_bzero(minishell->current->row, ft_strlen(minishell->current->row));
+			ft_strlcpy(minishell->current->row, minishell->current->old, ft_strlen(minishell->current->old) + 1);
+			minishell->current->index = ft_strlen(minishell->current->row);
+			write(1, "\n", 1);
+			write(1, getprompt(minishell), ft_strlen(getprompt(minishell)));
+			while (minishell->current->next)
+				minishell->current = minishell->current->next;
+			minishell->current->index = 0;
+		}
+		else
+			write(1, "\n", 1);
 	}
 	if (sig == 3)
 	{
