f692830b81abbc9d1158c29f66bc9a533158e209
scilla <scilla@student.42.fr>
2021-07-01 14:48:15 +0200

    fix double free dred

--- srcs/prompt/d_redirect.c
+++ srcs/prompt/d_redirect.c
@@ -1,28 +1,29 @@
 #include "../../includes/minishell.h"
-int	d_redirect2(char *res, char *tmp, char *eof)
+int	d_redirect2(char **res, char *eof)
 {
+	char *tmp;
+
 	get_prompt();
 	ft_bzero(minishell->current->row, ft_strlen(minishell->current->row));
 	minishell->current->index = 0;
 	hook_line(minishell);
 	if (!ft_strcmp(minishell->current->row, eof))
 	{
-		return (0) ;
+		return (0);
 	}
 	if (minishell->abort_dred)
 		return (0);
 	minishell->current->row[ft_strlen(minishell->current->row)] = '\n';
-	tmp = ft_strjoin(res, minishell->current->row);
-	free(res);
-	res = tmp;
+	tmp = ft_strjoin(*res, minishell->current->row);
+	free(*res);
+	*res = tmp;
 	return (1);
 }
+
 char	*d_redirect(char *eof)
 {
 	char	*res;
-	char	*tmp;
 
-	tmp = NULL;
 	res = malloc(sizeof(char));
 	*res = 0;
 	ft_new_history(&minishell->current);
@@ -30,7 +31,7 @@ char	*d_redirect(char *eof)
 	minishell->in_dred = 1;
 	while (1)
 	{
-		if (!d_redirect2(res, tmp, eof))
+		if (!d_redirect2(&res, eof))
 			break ;
 	}
 	if (!minishell->abort_dred)
