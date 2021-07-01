f0f6d95d48efa67e8155539d5fd198bcbea432d0
vbuonvin <vbuonvin@e3r6p7.42roma.it>
2021-07-01 16:38:11 +0200

    errno for cmd

--- srcs/executor/error.c
+++ srcs/executor/error.c
@@ -3,7 +3,10 @@
 void	ft_error(int errnum, char *str, int cmd_flag)
 {
 	if(cmd_flag > 0)
+	{
 		ft_printf_fd(2, "Conchiglia: %s: command not found\n", str);
+		errno = 127;
+	}
 	else if (str)
 		ft_printf_fd(2, "Conchiglia: %s: %s\n", str, strerror(errnum));
 	else
