f59c7cc482a14817a595ad30c07ced4c27819b58
scilla <scilla@student.42.fr>
2021-07-06 17:24:29 +0200

    up

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: scilla <scilla@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 16:53:17 by scilla            #+#    #+#             */
-/*   Updated: 2021/07/06 17:23:55 by scilla           ###   ########.fr       */
+/*   Updated: 2021/07/06 17:24:26 by scilla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -146,8 +146,7 @@ t_cmd	**start_parsing(const char *cmd)
 	{
 		printf("FREEEEEEEEEEEEEEEEEEE\n");
 		free(cv->comm);
-		free(cv->comm->eof);
-		/*
+		
 		i = 0;
 		while (*(cv->arr + i))
 		{
@@ -155,7 +154,7 @@ t_cmd	**start_parsing(const char *cmd)
 			i++;
 		}
 		free(cv->arr);
-		*/
+		
 	}
 	
 	res = cv->cmd_arr;
